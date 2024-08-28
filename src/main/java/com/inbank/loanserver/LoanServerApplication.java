3b2aca8562faa505d7b1e526305c50c0a1a17449
Vinod John <vinodjohn@MacBook-Air.local>
2024-08-29 02:19:00 +0300

    Initial commit

---
+++ src/main/java/com/inbank/loanserver/LoanServerApplication.java
@@ -0,0 +1,13 @@
+package com.inbank.loanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inbank/loanserver/LoanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inbank.loanserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
