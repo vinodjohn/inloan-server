30d0f514b77eae2c2fd68ce19f45766def64f207
vinodjohn <vinodjohn56@gmail.com>
2024-08-31 23:23:47 +0300

    Added Audit

--- src/main/java/com/inbank/loanserver/configurations/AuditConfiguration.java
+++ src/main/java/com/inbank/loanserver/configurations/AuditConfiguration.java
@@ -1,5 +1,6 @@
 package com.inbank.loanserver.configurations;
 
+import com.inbank.loanserver.handlers.AuditorAwareHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
