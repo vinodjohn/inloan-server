3db12826fb17958a658961b82777fa9be84a4262
vinodjohn <vinodjohn56@gmail.com>
2024-09-01 00:17:12 +0300

    Added CORS config

---
+++ src/main/java/com/inbank/loanserver/configurations/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.inbank.loanserver.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration for CORS
+ *
+ * @author vinodjohn
+ * @created 01.09.2024
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT");
+    }
+}
